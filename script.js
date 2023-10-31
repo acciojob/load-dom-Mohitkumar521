a97e32ca80a536a4d278bdef7b53fb7bc461f3b7
Mohit Kumar <109014039+Mohitkumar521@users.noreply.github.com>
2023-10-31 14:54:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-   function verifyDOMLoaded() {
-            console.log("1) Loads DOM content successfully");
-        }
-
-        // Function to verify that the body element is empty
-        function verifyEmptyBody() {
-            const body = document.body;
-
-            if (body.childNodes.length === 0) {
-                console.log("2) Verifies that the body element is empty");
-            }
-        }
-
-        // Add an event listener for DOMContentLoaded
-        document.addEventListener("DOMContentLoaded", function() {
-            verifyDOMLoaded();
-            verifyEmptyBody();
-        });
+   window.onload = function() {
+  document.body.innerHTML = "DOM load success";
+}
